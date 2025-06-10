f0215f8b3d584072573fb80bed416a0384adb36f
Priya Patil <priyapatil8277@gmail.com>
2025-06-10 14:03:39 +0530

    Update script.js

--- todo-List/script.js
+++ todo-List/script.js
@@ -1,3 +1,4 @@
+
 const inputBox=document.getElementById("input-box");
 const input=document.getElementById("input");
 const button=document.getElementById("btn");
@@ -11,18 +12,47 @@ function display()
 
     todos.forEach((todo,index)=>{
         const div=document.createElement("div");
-
+        const btnDiv=document.createElement("div");
         const p=document.createElement("p");
-        const deleteButton=document.createElement("Delete");
+        const deleteButton=document.createElement("button");
+        const editButton=document.createElement("button");
 
         div.className="d-flex justify-content-between my-3";
         deleteButton.className="btn btn-danger";
+        editButton.className="btn btn-warning";
+        
         p.textContent=todo;
         deleteButton.textContent="Delete";
+        editButton.textContent="Edit";
+
+        deleteButton.addEventListener('click',()=>{
+          deleteTodo(index);
+        });
+
+        editButton.addEventListener('click',()=>{
+          editTodo(index);
+        })
+        
+        btnDiv.appendChild(deleteButton);
+        btnDiv.appendChild(editButton);
+        
         div.appendChild(p);
-        div.appendChild(deleteButton);
+        div.appendChild(btnDiv);
+
         outputBox.appendChild(div);
     })
+}
+
+function deleteTodo(index)
+{
+  todos.splice(index,1);
+  display();
+}
+
+function editTodo(index)
+{
+  input.value=todos[index];
+  todos.splice(index,1);
 }
 
 button.addEventListener('click',()=>{
