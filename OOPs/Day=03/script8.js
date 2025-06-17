38a7f927fcef3e816aac668053faa459ae8f50e6
Priya Patil <priyapatil8277@gmail.com>
2025-06-17 11:42:23 +0530

    Update script8.js

--- OOPs/Day=03/script8.js
+++ OOPs/Day=03/script8.js
@@ -4,43 +4,43 @@
 // Implement a parameterized constructor for the `Person` class. 
 // Create N number of objects using this constructor and display the details.
 
-class Person
+class Person//create a class name person.
 {
 
-    #name;
-    #age;
-    #address;
+    #name;//create a private attribute name.
+    #age;//create a private attribute age.
+    #address;//create a private attribute address.
 
-    constructor(name,age,address) 
+    constructor(name,age,address) //create a constructor with parameter (name,age,address) passed to it.
     {
-        this.#name=name;
-        this.#age=age;
-        this.#address=address;
+        this.#name=name;//assign the value of local variable name to global private variable of name with (this.) keyword.//this keyword is used for global variable.
+        this.#age=age;///assign the value of local variable age to global private variable of age with (this.) keyword.//this keyword is used for global variable.
+        this.#address=address;//assign the value of local variable address to global private variable of address with (this.) keyword.//this keyword is used for global variable.
     }
 
-    set(name,age,address)
+    set(name,age,address)//create a method set with paramter (name,age,address) passed to it.
     {
-        this.#name=name;
-        this.#age=age;
-        this.#address=address;
+        this.#name=name;///assign the value of local variable name to global private variable of name with (this.) keyword.//this keyword is used for global variable.
+        this.#age=age;//assign the value of local variable age to global private variable of age with (this.) keyword.//this keyword is used for global variable.
+        this.#address=address;//assign the value of local variable address to global private variable of address with (this.) keyword.//this keyword is used for global variable.
     }
 
-     get()
+     get()//create a method get.
     {
-        return;
+        return;//
     }
     
 
-    display()
+    display()//create a method display to display person details.
     {
-        document.writeln("<b>Name is </b>:"+this.#name+"<br>");
-        document.writeln("<b>Age </b>:"+this.#age+"<br>");
-        document.writeln("<b>Address</b> :"+this.#address+"<br>");
+        document.writeln("<b>Name is </b>:"+this.#name+"<br>");//print person name.
+        document.writeln("<b>Age </b>:"+this.#age+"<br>");//print person age.
+        document.writeln("<b>Address</b> :"+this.#address+"<br>");//print person address.
     }
 }
 
-const p1 = [
-    new Person("priya",20,"Surat"),
+const p1 = [ // create an array of 5 person 
+    new Person("priya",20,"Surat"),//create 1 object with values.
     new Person("riya",18,"Surat"),
     new Person("muskan",19,"Surat"),
     new Person("megha",20,"Surat"),
@@ -48,8 +48,8 @@ const p1 = [
 
 ];
 
-for(let i=0; i<p1.length;i++)
+for(let i=0; i<p1.length;i++)// using a for loop to display all object from array.
 {
-    document.writeln("<br>");
-    p1[i].display();
+    document.writeln("<br>");//print to move the content to the next line.
+    p1[i].display();//call the method display for each object.
 }
